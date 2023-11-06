c629cea022f681d737c3e3ba3176a5917ed3c04f
mnuland <mnuland@nikhef.nl>
2023-11-06 11:56:20 +0100

    Add layout (only title)

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import dash
 
 app = dash.Dash(__name__, title="2x2 event display")
 
+app.layout = dash.html.Div(
+    [dash.html.H1(children="2x2 event display", style={"textAlign": "center"})]
+)
+
 # Run the app
 if __name__ == "__main__":
     app.run(debug=True)
